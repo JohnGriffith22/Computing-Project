40d4884375f85ccf09f6c496144e534b598dae08
JohnGriffith22 <johng1803@outlook.com>
2025-10-10 20:02:46 +0100

    fourth commit

--- test.py
+++ test.py
@@ -4,3 +4,15 @@ testing = numpy.pi
 test = 'Hello Wordle'
 
 change = "I changed it"
+
+numpy.random.rand = 'very useful'
+
+#T. Gu´enault, Statistical Physics, Springer (2007); Durham University Library - available online.
+
+#D. Frenkel and B. Smit, Understanding Molecular Simulation from Algorithms to Applications, Academic
+#Press (2002); Durham University Library - available online.
+
+
+#C.P. Royall, P. Charbonneau, M. Dijkstra, J. Russo, F. Smallenburg, T. Speck, C. Valeriani, Colloidal
+#hard spheres: Triumphs, challenges, and mysteries, Rev. Mod. Phys., 96, 4, 2024; 10.1103/RevMod-
+#Phys.96.045003.
